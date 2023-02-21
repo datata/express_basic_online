1a4942ccf854a56130741f36c9916c2e87aed242
Dani <d.tarazoma@geekshubs.com>
2023-02-21 14:14:04 +0100

    nuevo endpoint welcome creado

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ const app = express();
 
 const port = 3000;
 
+app.get('/welcome', (request, response) => {
+    return response.send('Welcome');
+});
+
 app.listen(port, () => console.log('Server is running'));
