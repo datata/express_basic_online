fdb35545142442c231015da040ecad06b84ecfb3
Dani <d.tarazoma@geekshubs.com>
2023-02-21 14:04:15 +0100

    declarada la variable port 3000

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
 
+const port = 3000;
 
-app.listen(3000, () => console.log('Server is running'));
+app.listen(port, () => console.log('Server is running'));
